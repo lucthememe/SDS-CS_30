0a89f17f82cfb1da06df0554c2af12afcd94bdf7
lucthememe <luc@coutus.net>
2024-11-26 14:17:17 -0600

    added  start of project
    added player var
    added station types
    added class for making stations
    added cargos

--- sketch.js
+++ sketch.js
@@ -1,11 +1,58 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// star trader
+// Luc Coutu
+// start date 26/11/2024
+
+let player = {
+  curantPlayerstation: null,
+  cargoHoldSize: 10,
+  iron: 0,
+  copper: 0,
+  tungsten: 0,
+  water: 0,
+  aluminum: 0,
+  processedFood: 0,
+  quantainium: 0,
+  constructionMaterials: 0,
+  RMC: 0,
+  distilledSpirits: 0,
+};
+
+const cargo = {
+  iron: "iron",
+  copper: "copper",
+  tungsten: "tungsten",
+  water: "water",
+  aluminum: "aluminum",
+  processedFood: "processedFood",
+  quantainium: "quantainium",
+  constructionMaterials: "constructionMaterials",
+  RMC: "RMC",
+  distilledSpirits: "distilledSpirits",
+};
+
+const stationType ={
+  smelter: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.copper, cargo.tungsten, cargo.aluminum]],
+  scrapYard: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.RMC, cargo.constructionMaterials]]
+};
+
+let genratedstations = [];
 
 
+class Createstation {
+  constructor(name, x, y, preset, productionType){
+    this.name = name;
+    this.numberOfCargos = random(5, 10);
+    this.x = x;
+    this.y = y;
+    this.preset = preset;
+    this.productionType = productionType;
+
+  }
+
+  CreateCargoList(numOfCargo){
+    makeCargoList(numOfCargo);
+  }
+};
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -13,4 +60,11 @@ function setup() {
 function draw() {
   background(220);
   circle(mouseX, mouseY, 100);
+}
+
+function makeCargoList(cargoAmount, proType){
+  let cargoArray = [];
+  for (cargoAmount > 0; cargoAmount--;){
+    
+  }
 }
