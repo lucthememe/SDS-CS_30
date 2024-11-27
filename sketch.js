daca30d4318c701db4fbe1739c36e57cee1756da
lucthememe <luc@coutus.net>
2024-11-27 14:16:12 -0600

    added more stations worked on cargo list function

--- sketch.js
+++ sketch.js
@@ -28,20 +28,27 @@ const cargo = {
   constructionMaterials: "constructionMaterials",
   RMC: "RMC",
   distilledSpirits: "distilledSpirits",
+  agriculturalSupplies: "agriculturalSupplies",
+  compboard: "compboard",
+  waste: "waste"
 };
 
 const stationType ={
-  smelter: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.copper, cargo.tungsten, cargo.aluminum]],
-  scrapYard: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.RMC, cargo.constructionMaterials]]
+  smelter: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.copper, cargo.tungsten, cargo.aluminum, cargo.waste]],
+  shipYard: [[cargo.RMC, cargo.constructionMaterials, cargo.compboard, cargo.tungsten], [cargo.waste]],
+  scrapYard: [[cargo.water, cargo.processedFood, cargo.distilledSpirits], [cargo.iron, cargo.RMC, cargo.constructionMaterials, cargo.waste]],
+  scienceLab: [[cargo.water, cargo.processedFood, cargo.compboard], [cargo.waste, cargo.quantainium, cargo.agriculturalSupplies]],
+  eggriculture: [[cargo.waste, cargo.constructionMaterials, cargo.agriculturalSupplies], [cargo.water, cargo.processedFood, cargo.distilledSpirits, cargo.waste]],
+  manufacturingHub: [[cargo.iron, cargo.copper, cargo.aluminum], [cargo.compboard, cargo.waste]],
+
 };
 
 let genratedstations = [];
 
 
-class Createstation {
+class CreateStation {
   constructor(name, x, y, preset, productionType){
     this.name = name;
-    this.numberOfCargos = random(5, 10);
     this.x = x;
     this.y = y;
     this.preset = preset;
@@ -49,8 +56,8 @@ class Createstation {
 
   }
 
-  CreateCargoList(numOfCargo){
-    makeCargoList(numOfCargo);
+  CreateCargoList(){
+    makeCargoList(this.productionType);
   }
 };
 function setup() {
@@ -62,9 +69,21 @@ function draw() {
   circle(mouseX, mouseY, 100);
 }
 
-function makeCargoList(cargoAmount, proType){
-  let cargoArray = [];
-  for (cargoAmount > 0; cargoAmount--;){
-    
+function makeCargoList(proType){
+  let cargoArray = [[][]];
+  let maxCargoBuyAmount;
+  let maxCargoSellAmount;
+  let cargoBuyAmount;
+  let cargoSellAmount;
+
+  MaxcargoBuyAmount = protype[0].length();
+  MaxcargoSellAmount = protype[1].length();
+  cargoBuyAmount = rand(2, maxCargoBuyAmount);
+  cargoSellAmount = rand(2, maxCargoSellAmount);
+
+  for (cargoBuyAmount > 0; cargoBuyAmount--;){
+    cargoArray[0].push(proType[0][rand(maxCargoBuyAmount)]);
   }
-}
+  
+};
+
